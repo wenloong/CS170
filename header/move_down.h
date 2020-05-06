3f10907633eb7b8f42117843cf031fcc1b3418ef
Clifford Ng <wenloongng@gmail.com>
2020-05-06 05:53:45 -0700

    Update move_down.h

--- header/move_down.h
+++ header/move_down.h
@@ -1,5 +1,17 @@
 #ifndef MOVE_DOWN_H
 #define MOVE_DOWN_H
 
+#include "operator.h"
+#include "problem.h"
+
+class Move_Down: public Operator {
+   public:
+      Move_Down(Problem* puzzle): Operator(puzzle) {
+         this->puzzle = puzzle;
+      }
+
+   private:
+      Problem* puzzle;
+};
 
 #endif
