0e13304bf1cd3481705677737e1811bf4648c8e5
Clifford Ng <wenloongng@gmail.com>
2020-06-01 21:55:00 -0700

    Update main.cpp

--- project-2/main.cpp
+++ project-2/main.cpp
@@ -1,121 +1,126 @@
+#include <algorithm>
 #include <iostream>
-#include <cmath>
+#include <stdlib.h>
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <algorithm>
-#include <stdlib.h>
+#include <cmath>
+
 using namespace std;
 
 double leave_one_out(vector<vector<double>> data, vector<int> currentFeatures ,int newFeature, bool isForward) {
-	int numCorrect = 0; // number of correct classifications
-	double tmp, min_dist, dist = 0;
-	vector <double> testingSet;
-	vector <double> nearest; // nearest neighbor (closest point in training set)
+   int numCorrect = 0; // number of correct classifications
+   double tmp, min_dist, dist = 0;
+   vector <double> testingSet;
+   vector <double> nearest; // nearest neighbor (closest point in training set)
 	
-	for (int i = 0; i < data.size(); i++) {	
-		testingSet = data[i]; 
-		min_dist = 1000000;
-		dist = 0;
+   for (int i = 0; i < data.size(); i++) {	
+      testingSet = data[i]; 
+      min_dist = 1000000;
+      dist = 0;
 
-		for (int j = 0; j < data.size(); j++) { 
-			if (j != i) { 
-				tmp = 0;
-				for (int k = 0; k < currentFeatures.size(); k++)  
-					tmp += (pow(testingSet[currentFeatures[k]] - data[j][currentFeatures[k]], 2));
-				if (isForward)
-					tmp += (pow(testingSet[newFeature] - data[j][newFeature], 2));
-				dist = sqrt(tmp);
-				if (dist < min_dist) { 
-					min_dist = dist;
-					nearest = data[j];
-				}
-			}	
-		}	
-		
-		// if predicted/actual classifications are the same, increment numCorrect
-		if (nearest[0] == 1 && testingSet[0] == 1) 
-			++numCorrect;
-		else if (nearest[0] == 2 && testingSet[0] == 2)
-			++numCorrect;
-	}
+      for (int j = 0; j < data.size(); j++) { 
+         if (j != i) { 
+            tmp = 0;
+		 
+            for (int k = 0; k < currentFeatures.size(); k++) {
+               tmp += (pow(testingSet[currentFeatures[k]] - data[j][currentFeatures[k]], 2));
+	    }
+            if (isForward) {
+                  tmp += (pow(testingSet[newFeature] - data[j][newFeature], 2));
+	    }
+            dist = sqrt(tmp);
+		 
+            if (dist < min_dist) { 
+               min_dist = dist;
+               nearest = data[j];
+            }
+	 }	
+      }	
 
-	return (double)numCorrect / (double)data.size();
+      // if predicted/actual classifications are the same, increment numCorrect
+      if (nearest[0] == 1 && testingSet[0] == 1) 
+         ++numCorrect;
+      else if (nearest[0] == 2 && testingSet[0] == 2)
+         ++numCorrect;
+   }
+   return (double)numCorrect / (double)data.size();
 }
 
 void nearest_neighbour(vector<vector<double>> data, int numFeatures) {
-	vector<int> currentFeatures;
+   vector<int> currentFeatures;
 
-	for (int i = 1; i < numFeatures; i++)
-		currentFeatures.push_back(i);
+   for (int i = 1; i < numFeatures; i++)
+      currentFeatures.push_back(i);
 	
-	double accuracy = leave_one_out(data,currentFeatures,0,false);
+   double accuracy = leave_one_out(data,currentFeatures,0,false);
 				
-	cout << "Running nearest neighbor with all " << numFeatures - 1 << " features, using \"leaving-one-out\" evaluation, I get an accuracy of ";
+   cout << "Running nearest neighbor with all " << numFeatures - 1 << " features, using \"leaving-one-out\" evaluation, I get an accuracy of ";
    cout << leave_one_out(data,currentFeatures,0,false) << "%" << endl << endl;
 }
 
 void forwardSelection(vector< vector<double> > data) {
-	vector<int> currentFeatures; // current set of features being tested
-	vector<int> bestFeatures; // set of features with highest accuracy 
-	double accuracy = 0; 
-	double bestAccuracy = 0; // highest accuracy to a certain point
-	double maxAccuracy = 0; // highest accuracy overall
-	int addedFeature;
+   vector<int> currentFeatures; // current set of features being tested
+   vector<int> bestFeatures; // set of features with highest accuracy 
+   double accuracy = 0; 
+   double bestAccuracy = 0; // highest accuracy to a certain point
+   double maxAccuracy = 0; // highest accuracy overall
+   int addedFeature;
 	
-	for (int i = 1; i < data.at(0).size(); i++) {
-		bestAccuracy = 0;
-		for (int j = 1; j < data.at(0).size(); j++) { 
-			if (std::find(currentFeatures.begin(), currentFeatures.end(), j) == currentFeatures.end()) {
-				accuracy = leave_one_out(data,currentFeatures, j, true);
+   for (int i = 1; i < data.at(0).size(); i++) {
+      bestAccuracy = 0;
+      
+      for (int j = 1; j < data.at(0).size(); j++) { 
+         if (find(currentFeatures.begin(), currentFeatures.end(), j) == currentFeatures.end()) {
+            accuracy = leave_one_out(data,currentFeatures, j, true);
 				
-				cout << "\tUsing feature(s) {";
-				for (int k = 0; k < currentFeatures.size(); k++) 
-					cout << currentFeatures.at(k) << ",";
-				cout << j << "} accuracy is ";
+            cout << "\tUsing feature(s) {";
+            for (int k = 0; k < currentFeatures.size(); k++) 
+               cout << currentFeatures.at(k) << ",";
+            cout << j << "} accuracy is ";
+            cout << accuracy * 100 << "%" << endl;
 
-				cout << accuracy * 100 << "%" << endl;
-				if (accuracy > bestAccuracy) {
-					addedFeature = j;
-					bestAccuracy = accuracy;
-				}
-			}
-		}
-		currentFeatures.push_back(addedFeature);
-		cout << endl;
-		if (bestAccuracy > maxAccuracy) {
-			maxAccuracy = bestAccuracy;
-			bestFeatures = currentFeatures;
-		}
-		else {
-			cout << "(Warning, accuracy has decreased! Continuing search in case of local maxima)" << endl;
-		}
-		cout << "Feature set {";
-		for (int k = 0; k < currentFeatures.size() - 1; k++)
-			cout << currentFeatures.at(k) << ",";
-		cout << currentFeatures.at(currentFeatures.size() - 1);
-		cout <<"} was best, accuracy is ";
-		cout << bestAccuracy * 100 << "%" << endl << endl;
-	}
+            if (accuracy > bestAccuracy) {
+               addedFeature = j;
+               bestAccuracy = accuracy;
+            }
+         }
+      }
+	   
+      currentFeatures.push_back(addedFeature);
+      cout << endl;
+
+      if (bestAccuracy > maxAccuracy) {
+         maxAccuracy = bestAccuracy;
+         bestFeatures = currentFeatures;
+      } else {
+         cout << "(Warning, accuracy has decreased! Continuing search in case of local maxima)" << endl;
+      }
+	   
+      cout << "Feature set {";
+      for (int k = 0; k < currentFeatures.size() - 1; k++)
+         cout << currentFeatures.at(k) << ",";
+      cout << currentFeatures.at(currentFeatures.size() - 1);
+      cout <<"} was best, accuracy is ";
+      cout << bestAccuracy * 100 << "%" << endl << endl;
+   }
 	
-	cout << "Finished search!! The best feature subset is {";
-	for (int k = 0; k < bestFeatures.size() - 1; k++)
-		cout << bestFeatures.at(k) << ",";
-	cout << bestFeatures.at(bestFeatures.size() - 1);
-	cout << "}, which has an accuracy of " << maxAccuracy * 100 << "%" << endl;
-	
-	return;
+   cout << "Finished search!! The best feature subset is {";
+   for (int k = 0; k < bestFeatures.size() - 1; k++)
+      cout << bestFeatures.at(k) << ",";
+   cout << bestFeatures.at(bestFeatures.size() - 1);
+   cout << "}, which has an accuracy of " << maxAccuracy * 100 << "%" << endl;
 }
 
 // helper function for backward elimination (removes feature from vector)
 vector <int> removeFeature(vector<int> currentFeatures, int removedFeature) {
-	for (int i = 0; i < currentFeatures.size(); i++) {
-		if (currentFeatures.at(i) == removedFeature) {
-			currentFeatures.erase(currentFeatures.begin() + i);
-			return currentFeatures;
-		}
-	}
-	return currentFeatures;
+   for (int i = 0; i < currentFeatures.size(); i++) {
+      if (currentFeatures.at(i) == removedFeature) {
+         currentFeatures.erase(currentFeatures.begin() + i);
+         return currentFeatures;
+      }
+   }
+   return currentFeatures;
 }
 
 void backwardElim(vector< vector<double> > data) {
@@ -216,12 +221,8 @@ int main() {
 	
 	if (choice == 1) {
 		forwardSelection(dataset);
-	}
-	else if (choice == 2) {
+	} else if (choice == 2) {
 		backwardElim(dataset);
-	}
-	else if (choice == 3) {
-	//	mySearch(dataset);
 	}
 	return 0;
 }
