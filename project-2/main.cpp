4bff0eb4384b4c6360e248c0e64ccbb3c23691da
Clifford Ng <wenloongng@gmail.com>
2020-06-01 23:43:07 -0700

    out of range error

--- project-2/main.cpp
+++ project-2/main.cpp
@@ -233,9 +233,8 @@ void backward_elimination(vector<vector<double>> data) {
 	   
       for (int j = 1; j < data[0].size(); j++) { 
          if (find(currentFeatures.begin(), currentFeatures.end(), j) != currentFeatures.end()) {
-            vector<int> temp;
-            
-            temp.erase(remove(temp.begin(), temp.end(), j), temp.end());
+            vector<int> temp = removedFeature(currentFeatures, removedFeature);
+
             cout << "Using feature(s) {";
             for (int k = 0; k < temp.size() - 1; k++)
                cout << temp.at(k) << ",";
